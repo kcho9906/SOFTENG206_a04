53a659f742cc38d13ac0ba1e9c37ca7b683c37d9
Charles <cpat430@aucklanduni.ac.nz>
2019-10-10 10:11:52 +1300

    Added a terminal method and an addConfirmationAlert method to the method helper

--- a04/src/application/MethodHelper.java
+++ a04/src/application/MethodHelper.java
@@ -5,10 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Optional;
 
 public class MethodHelper {
 
@@ -19,4 +22,70 @@ public class MethodHelper {
         window.setScene(newScene);
         window.show();
     }
+
+    public boolean addConfirmationAlert(String title, String contentText) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setContentText(contentText);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * This method acts as the bash command line where the input command
+     * runs in the bash terminal
+     * @param command
+     * @return
+     */
+    public static String command(String command) {
+
+        try {
+
+            String output = null;
+
+            ProcessBuilder pb = new ProcessBuilder("bash", "-c", command);
+
+            Process process = pb.start();
+
+            BufferedReader stdout = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            BufferedReader stderr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+            int exitStatus = process.waitFor();
+
+            if (exitStatus == 0) {
+
+                String line;
+                String text = "";
+                while ((line = stdout.readLine()) != null) {
+
+                    text = text + "\n" + line;
+                }
+                output = text;
+            } else {
+
+                String line;
+                while ((line = stderr.readLine()) != null) {
+
+                    output = line;
+                }
+            }
+
+            if (output == null) {
+
+                return "No output";
+            }
+
+            return output;
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+        return "Error";
+    }
+
 }
