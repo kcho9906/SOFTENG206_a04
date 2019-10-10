03d61df70fca00730f439ecbd558c62542c1d37e
Charles <cpat430@aucklanduni.ac.nz>
2019-10-10 13:47:34 +1300

    Added search button functionality

--- a04/src/application/TerminalWorker.java
+++ a04/src/application/TerminalWorker.java
@@ -1,4 +1,23 @@
 package application;
 
-public class TerminalWorker {
+import javafx.concurrent.Task;
+
+public class TerminalWorker extends Task<String> {
+
+    private String _command;
+    private MethodHelper methodHelper = new MethodHelper();
+
+    public TerminalWorker(String command) {
+
+        this._command = command;
+    }
+
+    @Override
+    protected String call() throws Exception {
+
+        String output = methodHelper.command(_command);
+        output = " " + output;
+
+        return output;
+    }
 }
